f4111a8b1a2bad59d2cf110f074dc3b9e1fc1cb6
Soubhik-10 <soubhiksmp2004@gmail.com>
2025-11-04 17:14:12 +0530

    fix index

--- crates/evm/src/eth/block.rs
+++ crates/evm/src/eth/block.rs
@@ -276,7 +276,7 @@ where
                             alloy_bal.push(
                                 AccountChanges::new(withdrawal.address).with_balance_change(
                                     BalanceChange::new(
-                                        self.evm.db().bal_state.bal_index,
+                                        self.evm.db().bal_state.bal_index - 1,
                                         U256::from(final_balance),
                                     ),
                                 ),
